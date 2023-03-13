a72747c4a64248736c948511691f06f3cbf5f8da
Tarsis <br.tmvdl@gmail.com>
2023-03-13 10:40:02 -0300

    Fixing scripts

--- env/down.sh
+++ env/down.sh
@@ -1,5 +1,1 @@
 docker-compose -f ./docker-compose.yaml down --remove-orphans --rmi all 
-
-rm -rf data/ 
-
-rm -rf logs/ 
--- env/prune.sh
+++ env/prune.sh
@@ -1,5 +1,1 @@
 docker-compose -f docker-compose.yaml down --remove-orphans --rmi all 
-
-sudo rm -rf ./data
-
-sudo rm -rf ./logs
